462d298f94649859d35f1f260864bcfad9e792f9
Erik Stackelberg <erikstackelberg@gmail.com>
2017-12-11 22:01:26 +0100

    major update, new content anent and image grid library

--- script.js
+++
@@ -1,8 +0,0 @@
-// external js: masonry.pkgd.js, imagesloaded.pkgd.js
-
-// vanilla JS
-var msnry = new Masonry(".grid", {
-	itemSelector: ".grid-item",
-	columnWidth: ".grid-sizer",
-	percentPosition: true
-});
