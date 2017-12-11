cac643c69940eedeee72f4c416f453f1de05c38d
Erik Stackelberg <erikstackelberg@gmail.com>
2017-12-11 16:21:33 +0100

    added image grid

---
+++ script.js
@@ -0,0 +1,8 @@
+// external js: masonry.pkgd.js, imagesloaded.pkgd.js
+
+// vanilla JS
+var msnry = new Masonry(".grid", {
+	itemSelector: ".grid-item",
+	columnWidth: ".grid-sizer",
+	percentPosition: true
+});
